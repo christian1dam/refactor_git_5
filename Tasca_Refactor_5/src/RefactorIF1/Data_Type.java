6df2f79f3380a00a5800deb80764ee1eae77e1a6
Christian <christianrmch@outlook.es>
2023-05-15 10:38:51 +0200

    Revert "ArrayList<Integer> codes() creado para almacenar los códigos por defecto"
    This reverts commit 8224112d0dcd334f15b888d96fc9f1f4fcab8c84.

--- Tasca_Refactor_5/src/RefactorIF1/Data_Type.java
+++ Tasca_Refactor_5/src/RefactorIF1/Data_Type.java
@@ -1,25 +1,6 @@
 package RefactorIF1;
 
-import java.lang.reflect.Array;
-import java.util.ArrayList;
-import java.util.List;
-
 public class Data_Type {
-    private List<Integer> codes = new ArrayList<Integer>(){{
-        add(20); add(21);
-        add(23); add(24);
-        add(26); add(700);
-        add(701); add(790);
-        add(1700); add(2202);
-        add(2203); add(2204);
-        add(2205); add(2206);
-        add(3734); add(3769);
-        add(12396); add(702);
-        add(1082); add(1083);
-        add(1114); add(1184);
-        add(1266); add(12403);
-    }};
-
     public static void main(String[] args) {
         System.out.println(Data_Type.gd(20));
         System.out.println(Data_Type.gd(702));
